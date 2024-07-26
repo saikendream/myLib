f205a72b33cd98bcad2a087db02ffdf15e281fe4
Jae <121826115+saikendream@users.noreply.github.com>
2024-07-26 14:11:53 -0300

    Wrote Book constructor

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 /* SHELF */
 
 const myLib = [];
+
+/* Book Info */
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.release = release;
+    this.sinopsis = sinopsis;
+    this.coverart = coverart;
+    this.progress = progress;
+    this.rating = rating;
+}
