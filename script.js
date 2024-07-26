be9fce6f550a55d6d70d25077f845294bd205434
Jae <121826115+saikendream@users.noreply.github.com>
2024-07-26 14:32:41 -0300

    Created examples

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const myLib = [];
 
 /* Book Info */
 
-function Book() {
+function Book(title, author, release, length, sinopsis, coverart, progress, rating) {
     this.title = title;
     this.author = author;
     this.release = release;
+    this.length = length;
     this.sinopsis = sinopsis;
     this.coverart = coverart;
     this.progress = progress;
     this.rating = rating;
 }
+
+/* Testing */
+
+const entry1 = new Book("The Hobbit", "J. R. R. Tolkien", "1937-09-21", 95356, "The Hobbit is set in Middle-earth and follows home-loving Bilbo Baggins, the hobbit of the title, who joins the wizard Gandalf and the thirteen dwarves of Thorin's Company, on a quest to reclaim the dwarves' home and treasure from the dragon Smaug. Bilbo's journey takes him from his peaceful rural surroundings into more sinister territory.", "https://m.media-amazon.com/images/I/712cDO7d73L._AC_UF1000,1000_QL80_.jpg", 95, 0);
+
+const entry2 = new Book("Alice's Adventures in Wonderland", "Lewis Carroll", "1865-11-01", 29610, "It details the story of a girl named Alice who falls through a rabbit hole into a fantasy world of anthropomorphic creatures.", "https://ik.imagekit.io/panmac/tr:f-auto,di-placeholder_portrait_aMjPtD9YZ.jpg,w-270/edition/9781447279990.jpg", 100, 4);
