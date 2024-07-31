d251a1095e58e6357fbae6eeef76ab9f9a65ba20
Jae <121826115+saikendream@users.noreply.github.com>
2024-07-30 23:39:33 -0300

    Deleted setTimeouts; Switched PROGRESS into HTML progress bars; Changed test-books' progress values

--- script.js
+++ script.js
@@ -18,10 +18,8 @@ function Book(title, author, release, length, sinopsis, coverart, progress, rati
 
 /* Testing */
 
-const entry1 = new Book("The Hobbit", "J. R. R. Tolkien", "1937-09-21", 95356, "The Hobbit is set in Middle-earth and follows home-loving Bilbo Baggins, the hobbit of the title, who joins the wizard Gandalf and the thirteen dwarves of Thorin's Company, on a quest to reclaim the dwarves' home and treasure from the dragon Smaug. Bilbo's journey takes him from his peaceful rural surroundings into more sinister territory.", "https://m.media-amazon.com/images/I/712cDO7d73L._AC_UF1000,1000_QL80_.jpg", 95, 0);
-setTimeout(10000);
-const entry2 = new Book("Alice's Adventures in Wonderland", "Lewis Carroll", "1865-11-01", 29610, "It details the story of a girl named Alice who falls through a rabbit hole into a fantasy world of anthropomorphic creatures.", "https://ik.imagekit.io/panmac/tr:f-auto,di-placeholder_portrait_aMjPtD9YZ.jpg,w-270/edition/9781447279990.jpg", 100, 4);
-setTimeout(10000);
+const entry1 = new Book("The Hobbit", "J. R. R. Tolkien", "1937-09-21", 95356, "The Hobbit is set in Middle-earth and follows home-loving Bilbo Baggins, the hobbit of the title, who joins the wizard Gandalf and the thirteen dwarves of Thorin's Company, on a quest to reclaim the dwarves' home and treasure from the dragon Smaug. Bilbo's journey takes him from his peaceful rural surroundings into more sinister territory.", "https://m.media-amazon.com/images/I/712cDO7d73L._AC_UF1000,1000_QL80_.jpg", 15, 0);
+const entry2 = new Book("Alice's Adventures in Wonderland", "Lewis Carroll", "1865-11-01", 29610, "It details the story of a girl named Alice who falls through a rabbit hole into a fantasy world of anthropomorphic creatures.", "https://ik.imagekit.io/panmac/tr:f-auto,di-placeholder_portrait_aMjPtD9YZ.jpg,w-270/edition/9781447279990.jpg", 65, 4);
 const entry3 = new Book("Anne of Green Gables", "L. M. Montgomery", "1908-06-13", 106294, "Set in the late 19th century, the novel recounts the adventures of an 11-year-old orphan girl Anne Shirley sent by mistake to two middle-aged siblings, Matthew and Marilla Cuthbert, who had originally intended to adopt a boy to help them on their farm in the fictional town of Avonlea in Prince Edward Island, Canada.", "https://m.media-amazon.com/images/I/81NDwdjGwSL._AC_UF1000,1000_QL80_.jpg", 100, 5);
 
 /* Add to Lib */
@@ -88,7 +86,11 @@ for(let i = 0; i < myLib.length; i++) {
     bookEntry.appendChild(bookTitle);
     bookDiv("author", "author", myLib[i].author, bookEntry);
     bookDiv("rate", "rate", myLib[i].rating, bookEntry);
-    bookDiv("progress", "progress", `${myLib[i].progress}%`, bookEntry);
+    const progress = document.createElement("progress");
+    progress.max = 100;
+    progress.value = myLib[i].progress;
+    progress.classList.add("progress");
+    bookEntry.appendChild(progress);
     cardButtons(bookEntry);
     shelf.appendChild(bookEntry);
 }
